944ecd29c40d7cffd980dfd9f02fb846a3f20b78
Robin Bradley <robinbradley210@gmail.com>
2023-11-14 10:13:54 -0700

    add data simulation and plot

---
+++ simulate-data-plot.R
@@ -0,0 +1,13 @@
+# Simulate and plot data
+# Robin Bradley
+# robinbradley210@gmail.com
+# Date created: 14 Nov 2023
+
+#simulate predictor variable
+x <- rnorm(n=100)
+
+#Simulate response variable with some noise
+y <- 2*x + rnorm(n=100, sd = 0.2)
+
+#Make plots
+plot(x = x, y=y)
